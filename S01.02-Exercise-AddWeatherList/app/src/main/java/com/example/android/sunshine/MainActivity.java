d06e6feb0f72d4de94c0b114eec099d81a7de68d
Higaskino Marcelo <massao@Higaskinos-MBP.local>
2017-04-02 20:57:31 -0300

    Update with resolution of exercises until before the Intents class

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,12 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
+        String[] dummyData = {"Sunny", "Cloudy", "Rainy", "Partially Rainy", "Partially Cloudy",
+        "Raining Dogs", "Snowing", "Rainbow", "Moody", "Lovely"};
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for(String data : dummyData) {
+            if(mWeatherTextView.getText().length() > 0) {
+                mWeatherTextView.append("\n\n\n");
+            }
+            mWeatherTextView.append(data);
+        }
     }
 }
---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by massao on 02.04.17.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutId = R.layout.forecast_list_item;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View view = layoutInflater.inflate(layoutId, parent, false);
+
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        holder.mWeatherTextView.setText(mWeatherData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWeatherData != null ? mWeatherData.length : 0;
+    }
+
+    public void setWeatherData(String[] newData) {
+        mWeatherData = newData;
+        notifyDataSetChanged();
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+    }
+}
